5078ce25fdff32d85a32d86e8be2c4ea29f6833d
Dmitriy1001 <dumatkachuk@gmail.com>
2021-11-12 14:39:27 +0200

    added display of similar movies on the movie page

--- movie_catalog/templatetags/movie_tags.py
+++ movie_catalog/templatetags/movie_tags.py
@@ -1,7 +1,7 @@
 from django import template
+from django.db.models import Avg
 
-from movie_catalog.models import Category
-
+from movie_catalog.models import Category, Movie
 
 register = template.Library()
 
@@ -9,3 +9,23 @@ register = template.Library()
 @register.simple_tag()
 def get_categories():
     return Category.objects.all()
+
+
+@register.simple_tag()
+def get_similar_movies(current_movie:Movie):
+    current_movie_genres = current_movie.genre.all()
+    similar_movies = (
+        Movie.objects.filter(
+            available=True,
+            moderation=True,
+            category=current_movie.category,
+            genre__in=current_movie_genres,
+        )
+        .exclude(slug=current_movie.slug)
+        .distinct()
+        .annotate(avg_rating=Avg('ratings__star__number'))
+    )
+    for movie in similar_movies:
+        similar_genres = filter(lambda genre: genre in current_movie_genres, movie.genre.all())
+        movie.similar_genres_count = len(list(similar_genres))
+    return sorted(similar_movies, key=lambda movie: -movie.similar_genres_count)[:6]
