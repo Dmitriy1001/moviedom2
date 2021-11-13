3c4a7d015676d48b19531de710d9556acfe6f679
Dmitriy1001 <dumatkachuk@gmail.com>
2021-11-13 15:49:39 +0200

    created model Profile

--- account/models.py
+++ account/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    avatar = models.ImageField(upload_to='users_avatars/', blank=True)
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профиль'
+
+    def __str__(self):
+        return self.user.username
+
